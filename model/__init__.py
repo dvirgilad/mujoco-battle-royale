822c35f0fb8cc6c129aebe81ba62d3afa534f992
dvirgilad <dvirg8@outlook.com>
2026-03-28 13:05:24 +0300

    Rendering

---
+++ model/__init__.py
@@ -0,0 +1,1 @@
+from .model import load_model, load_data, render
--- model/model.py
+++ model/model.py
@@ -1,13 +1,6 @@
 
 import mujoco
-xml = """
-<mujoco>
-  <worldbody>
-    <geom name="red_box" type="box" size=".2 .2 .2" rgba="1 0 0 1"/>
-    <geom name="green_sphere" pos=".2 .2 .2" size=".1" rgba="0 1 0 1"/>
-  </worldbody>
-</mujoco>
-"""
+import mediapy as media
 
 
 def load_model(model_string: str = "", model_path: str = "") -> mujoco.MjModel:
@@ -15,9 +8,18 @@ def load_model(model_string: str = "", model_path: str = "") -> mujoco.MjModel:
         model = mujoco.MjModel.from_xml_string(model_string)
     elif model_path:
         model = mujoco.MjModel.from_xml_path(model_path)
+    else:
+        raise ValueError("Either model_string or model_path must be provided.")
     return model
 
 
 def load_data(model: mujoco.MjModel) -> mujoco.MjData:
     data = mujoco.MjData(model)
     return data
+
+
+def render(model: mujoco.MjModel, data: mujoco.MjData, output_path: str = "output.png") -> None:
+    with mujoco.Renderer(model) as renderer:
+        mujoco.mj_forward(model, data)
+        renderer.update_scene(data)
+        media.write_image(output_path, renderer.render())
